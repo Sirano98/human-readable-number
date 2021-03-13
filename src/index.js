f02a2934e37f97160cdfe5f9d9f6d9a8dab7db32
Sirano98 <protsiuk1998@gmail.com>
2021-03-13 17:36:48 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    if (number) {
+        let arrayOfUnits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+        let arrayOfSecondTen = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+
+        let arrayOfTens = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+        let hundreds = Math.floor(number / 100);
+
+        let tens = Math.floor((number - hundreds * 100) / 10);
+
+        let units = number - hundreds * 100 - tens * 10;
+
+        let result = `${hundreds ? arrayOfUnits[hundreds - 1] + " hundred" : ""} `;
+
+        if (tens >= 2) {
+            result += `${arrayOfTens[tens - 1]} ${units ? arrayOfUnits[units - 1] : ""}`;
+            return result.trim();
+        } else if (tens) {
+            result += `${units ? arrayOfSecondTen[units - 1] : arrayOfTens[tens - 1]}`;
+            return result.trim();
+        }
+        result += `${units ? arrayOfUnits[units - 1] : ""}`
+        return result.trim();
+    };
+
+    return "zero";
 }
